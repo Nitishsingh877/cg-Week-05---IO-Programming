b99e6dee113dbc1208af379e4dcc0ec6fbf0c8ef
Nitish singh <nitishkumarsingh877@gmail.com>
2025-02-19 14:56:09 +0530

    👍[Nitish] add: added code for converting java objects in jsonarrays

---
+++ Day02_JSON_Data/src/main/java/com/bridgelabz/nitish/submissionofJSON/handson_practice_problems/readaJsonfile/javaobjecttojsonarray/ConvertObjectstoArray.java
@@ -0,0 +1,65 @@
+package com.bridgelabz.nitish.submissionofJSON.handson_practice_problems.readaJsonfile.javaobjecttojsonarray;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+public class ConvertObjectstoArray {
+    public static void main(String[] args) {
+        List<Student> studentList = new ArrayList<>();
+        Student student = new Student("nitish",20);
+        Student  student1 = new Student("Siddhu",90);
+        studentList.add(student);
+        studentList.add(student1);
+
+        JSONArray jsonArray = new JSONArray();
+
+        for(Student students : studentList){
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("name" , students.getName());
+            jsonObject.put("age" , students.getAge());
+            jsonArray.put(jsonObject);
+        }
+        System.out.println(jsonArray.toString(2));
+    }
+}
+
+class Student {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
